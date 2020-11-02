4b83d39bd857a1284f7aaf450dcbde6356b7d4fe
Elam Day <elam.day.friedland@gmail.com>
2020-11-01 16:49:59 -0800

    todo actually handle collisons and also do dtype conversion on ID?

--- merge.py
+++ merge.py
@@ -1,11 +1,19 @@
 import pandas as pd
 def merge(survey, history):
-    survey = pd.read_excel(survey)
-    history = pd.read_excel(history)
-    try:
-        merged = survey.merge(history, left_on= "Por favor complete con sus datos: - Número de identificación del estudio", right_on="Unnamed: 17")#, validate="1:1")
-    except:
-        print("collisions!")
-        merged = pd.DataFrame()
+    read = pd.read_excel(survey)
+    survey = read.copy()
+    read = pd.read_excel(history, skiprows=1).copy()
+    history = read.copy()
+    survey[["Por favor complete con sus datos: - Número de identificación del estudio"]] = survey["Por favor complete con sus datos: - Número de identificación del estudio"].astype('U')
+    history[["Por favor complete con sus datos: - Número de identificación del estudio"]] = history["Por favor complete con sus datos: - Número de identificación del estudio"].astype('U')
+    print(survey["Por favor complete con sus datos: - Número de identificación del estudio"])
+    print(history["Por favor complete con sus datos: - Número de identificación del estudio"])
+    #try:
+    #    merged = survey.merge(history, left_on="Por favor complete con sus datos: - Número de identificación del estudio", right_on="Por favor complete con sus datos: - Número de identificación del estudio")#, validate="1:1")
+    #except Exception as e:
+    print("collisions!")
+    print("Survey collisions: " + survey.duplicated(subset=["Por favor complete con sus datos: - Número de identificación del estudio"]))
+    print("History collisions: " + history.duplicated(subset=["Por favor complete con sus datos: - Número de identificación del estudio"]))
+    merged = pd.DataFrame()
     return merged
 merge("SurveyClean_cleaned.xlsx","HistoryClean.xlsx").to_excel("merged.xlsx")
