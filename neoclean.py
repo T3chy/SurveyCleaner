50824147a421a045e85e05f2d5e4f4368c167180
Elam Day <elam.day.friedland@gmail.com>
2020-11-24 14:53:41 -0800

    gonna refactor to more and smaller funcs

--- neoclean.py
+++ neoclean.py
@@ -19,50 +19,51 @@ class survey:
         self.flagged = []
         self.index = 0
         self.q = ""
+    def logChange(qtype, init, index, q, reason=""):
+        final = self.data.loc[index,q]
+        rid = self.data.loc[index,id]
+        if final == "":
+            final = "an empty string"
+        if str(init).lower() != str(final).lower():
+            self.changes.append(str(qtype).upper() + ": Replaced " + str(init) + " with " + str(final) + " at respondant with ID " + str(rid) " question " + str(q) + " " + reason)
+
     def possibleID(self,tmp, log=True):
-       # if re.search("[A-Z]{2}\d{4}", tmp) or re.search("[A-Z]{3}\d{4}", tmp):
         if re.search("[A-Z].*?\d{4}", str(tmp)):
             if log:
                 self.flagged.append(["POSSIBLE ID FOUND: " + str(tmp), "respondant # " + str(self.index), str(self.q)])
             return 1
         else:
             return 0
-    def cleanNumeric(self, qs,):
-        for index, respondant in self.data.iterrows():
-            self.index = index
-            for q in qs:
-                self.q = q
-                tmp = self.data.loc[index,q]
-                if self.possibleID(tmp):
-                    pass
-                elif isinstance(tmp, float) or isinstance(tmp, int) or str(tmp).isnumeric():
-                    pass
-                elif tmp == ".":
-                    self.data.loc[index,q] = ""
-                    self.changes.append("NUMERIC: Replaced . with an empty string at respondant" + str(self.data.loc[index,id]) + " question " + str(q))
-                else:
-                    self.data.loc[index,q] = self.data.loc[index,q].replace("O", "0")
-                    self.data.loc[index,q] = re.sub("\D", "", self.data.loc[index,q])
-                    self.changes.append("NUMERIC: Replaced " + tmp + " with " +  str(self.data.loc[index,q]) + " at respondant " + str(self.data.loc[index,id]) + " question " +  str(q)) # maybe don't log just lowercasing
-    def cleanBinary(self, qs):
-        for index, respondant in self.data.iterrows():
-            self.index = index
-            for q in qs:
-                self.q = q
-                tmp = str(self.data.loc[index,q])
-                if self.possibleID(tmp):
-                    continue
-                self.data.loc[index,q] = str(self.data.loc[index,q]).lower()
-                if "no" in self.data.loc[index,q] or "desemplead" in self.data.loc[index,q]: # ask Laura abt this one or "voluntaria" in self.data.loc[index,q]:
-                    self.data.loc[index,q] = "no"
-                elif "sí" in self.data.loc[index,q] or "si" in self.data.loc[index,q] or "emplead" in self.data.loc[index,q]:
-                    self.data.loc[index,q] = "si"
-                elif "voluntari" in self.data.loc[index,q]:
-                    self.data.loc[index,q]= "voluntaria"
-                else:
-                    self.flagged.append([ str(index), str(q), self.data.loc[index,q]])
-                if str(self.data.loc[index,q]).lower() != tmp.lower():
-                    self.changes.append("BINARY: Replaced " +  tmp + " with " + str(self.data.loc[index,q] + " at respondant" + str(self.data.loc[index,id]) + " question " + str(q)))
+    def cleanNumeric(self, q, index):
+        self.index = index
+        self.q = q
+        tmp = self.data.loc[index,q]
+        if self.possibleID(tmp):
+            return
+        elif isinstance(tmp, float) or isinstance(tmp, int) or str(tmp).isnumeric():
+            return
+        elif tmp == ".":
+            self.data.loc[index,q] = ""
+        else:
+            self.data.loc[index,q] = self.data.loc[index,q].replace("O", "0")
+            self.data.loc[index,q] = re.sub("\D", "", self.data.loc[index,q])
+        logChange("numeric", tmp, index, q)
+    def cleanBinary(self, q, index):
+        self.index = index
+        self.q = q
+        tmp = str(self.data.loc[index,q])
+        if self.possibleID(tmp):
+            return
+        self.data.loc[index,q] = str(self.data.loc[index,q]).lower()
+        if "no" in self.data.loc[index,q] or "desemplead" in self.data.loc[index,q]: # ask Laura abt this one or "voluntaria" in self.data.loc[index,q]:
+            self.data.loc[index,q] = "no"
+        elif "sí" in self.data.loc[index,q] or "si" in self.data.loc[index,q] or "emplead" in self.data.loc[index,q]:
+            self.data.loc[index,q] = "si"
+        elif "voluntari" in self.data.loc[index,q]:
+            self.data.loc[index,q]= "voluntaria"
+        else:
+            self.flagged.append([ str(index), str(q), self.data.loc[index,q]])
+        logChange("binary", tmp, index, q)
     def inferLoc(self,value):
         if self.possibleID(value):
             return 1
@@ -74,19 +75,18 @@ class survey:
             elif self.data[self.idx,post] in SF:
                return "SF" + value
         return 0
-    def cleanID(self):
-        self.q = id
-        for index, respondant in self.data.iterrows():
-            self.index = index
-            tmp = self.data.loc[index, id].upper()
-            nloc = self.inferLoc(respondant[id])
-            if self.possibleID(respondant[id], log=False) and nloc:
-                if nloc != tmp and nloc != 1:
-                    self.changes.append("ID: Replaced " + tmp + " with " +  nloc + " by infering location with zipcode " + respondant[post])
-                    self.data.loc[index,id] = nloc if nloc != 1 else tmp
-                else:
-                    self.data.loc[index, id] = ""
-                    self.changes.append("ID: Deleted " + tmp  + " because it is not a valid ID")
+    def cleanID(self, index):
+        self.index = index
+        self.q = q
+        tmp = self.data.loc[index, id].upper()
+        nloc = self.inferLoc(respondant[id])
+        if self.possibleID(respondant[id], log=False) and nloc:
+            if nloc != tmp and nloc != 1:
+                self.changes.append("ID: Replaced " + tmp + " with " +  nloc + " by infering location with zipcode " + respondant[post])
+                self.data.loc[index,id] = nloc if nloc != 1 else tmp
+            else:
+                self.data.loc[index, id] = ""
+                self.changes.append("ID: Deleted " + tmp  + " because it is not a valid ID")
     def removeDirectDupes(self):
         tmp= self.data
         self.data = self.data.drop_duplicates(b.columns.difference(["Response ID", "Duration (in seconds)", "Unnamed: 0", "Recorded Date", "Start Date", "End Date"]))
@@ -95,9 +95,7 @@ class survey:
         else:
             print("no direct duplicates!")
     def getDupePairs(self, dupes):
-         pairs= []
-         group = []
-         nans = []
+         pairs, group, nans = [], [], []
          tmp = ""
          for idx, row in dupes.iterrows():
              try:
@@ -138,11 +136,6 @@ class survey:
     def resolveIdDupes(self, other=None):
          self.removeDirectDupes()
          dupes = self.data[self.data.duplicated(subset=[id], keep=False)].sort_values(by=[id])
-         try:
-             otherdupes = other[other.duplicated(subset=[id], keep=False)]
-             otherdupes = otherdupes.groupby(by=[id])
-         except:
-             otherdupes = -1
          pairs, nans = self.getDupePairs(dupes)
          if pairs == [[]] and nans == []:
              print("No collisions detected!")
@@ -155,11 +148,9 @@ class survey:
                  if choice.lower() == "y" or choice.lower() == "yes":
                      if pairs != [[]]:
                          for pair in pairs:
-                             print("____________________________________________________________________________________")
                              print("Colliding id detected!")
                              n = 1
                              for bruh in pair:
-                                 print("____________________________________________________________________________________")
                                  print("Respondant " + str(n) + ":")
                                  print(bruh)
                                  n += 1
@@ -184,7 +175,6 @@ class survey:
                  else:
                     print("ok! Please resolve them in the original input xlsx, and rerun this script!")
     def handlenanID(self, nans):
-         print("____________________________________________________________________________________")
          print(str(len(nans)) + " nan ids detected!")
          n = 1
          for bruh in nans:
@@ -226,13 +216,9 @@ class survey:
         self.data.to_excel(self.name + "_cleaned.xlsx")
         if self.merged != "":
             self.merged.to_excel(self.name + "_merged.xlsx")
-        pd.DataFrame(self.flagged,columns=["Respondant", "question", "value"]).to_excel(self.name + "_flagged.xlsx")
-        with open(self.name + "_changes.txt", "w") as f:
-            for change in self.changes:
-                f.write(change)
-                f.write("\n")
-    def theWorks(self):
-        self.cleanBinary(surveyDefaultBinaryQs)
-        self.cleanNumeric(surveyDefaultNumericQs)
-        self.removeDirectDupes()
-        self.resolveIdDupes()
+        if self.flagged != "":
+            pd.DataFrame(self.flagged,columns=["Respondant", "question", "value"]).to_excel(self.name + "_flagged.xlsx")
+        if selfchanges != []:
+            with open(self.name + "_changes.txt", "w") as f:
+                for change in self.changes:
+                    f.write(change)
